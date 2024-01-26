8d461d3870c5f94381a0d873e843385bc18ab158
Peter Tribble <peter.tribble@gmail.com>
2024-01-26 12:17:05 +0000

    The overlay @ processing should allow overlay names to contain numbers

--- lib/OverlayFilter.rb
+++ lib/OverlayFilter.rb
@@ -2,6 +2,6 @@ class OverlayFilter < Nanoc::Filter
   identifier :overlay
 
   def run(content, params = {})
-    content.gsub(/@([a-z-]*)/, '<a href="overlay-\1.html">\1</a>')
+    content.gsub(/@([a-z0-9-]*)/, '<a href="overlay-\1.html">\1</a>')
   end
 end
